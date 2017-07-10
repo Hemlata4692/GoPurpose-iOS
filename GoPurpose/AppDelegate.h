bfeb702fa13b457ff716870264b4408550a9f6f5
Public Repo <hemlata@ranosys.com>
2017-07-10 12:52:29 +0530

    Project Setup.

---
+++ GoPurpose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoPurpose
+//
+//  Created by Hemlata Khajanchi on 10/07/17.
+//  Copyright © 2017 Hemlata Khajanchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoPurpose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoPurpose
+//
+//  Created by Hemlata Khajanchi on 10/07/17.
+//  Copyright © 2017 Hemlata Khajanchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
